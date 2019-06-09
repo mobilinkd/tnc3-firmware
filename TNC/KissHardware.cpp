ef6e0c7fd053880fd116517b2d4643dff8d41fd4
Rob Riggs <rob@pangalactic.org>
2019-06-08 23:06:06 -0500

    Update version to 1.1.3rc2

--- TNC/KissHardware.cpp
+++ TNC/KissHardware.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Rob Riggs <rob@mobilinkd.com>
+// Copyright 2018-2019 Rob Riggs <rob@mobilinkd.com>
 // All rights reserved.
 
 #include "KissHardware.hpp"
@@ -26,7 +26,7 @@ int powerOffViaUSB(void)
 
 namespace mobilinkd { namespace tnc { namespace kiss {
 
-const char FIRMWARE_VERSION[] = "1.1.3rc1";
+const char FIRMWARE_VERSION[] = "1.1.3rc2";
 const char HARDWARE_VERSION[] = "Mobilinkd TNC3 2.1.1";
 
 Hardware& settings()
