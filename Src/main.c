3f4ea71f5f3ff248ecc8758bf03354efc47ea38d
Rob Riggs <rob@pangalactic.org>
2021-02-18 20:23:29 -0600

    Increase watchdog scaler to 16 for a 2s timeout.

--- Src/main.c
+++ Src/main.c
@@ -959,7 +959,7 @@ static void MX_IWDG_Init(void)
 {
 
   hiwdg.Instance = IWDG;
-  hiwdg.Init.Prescaler = IWDG_PRESCALER_4;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_16;
   hiwdg.Init.Window = 4095;
   hiwdg.Init.Reload = 4095;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
