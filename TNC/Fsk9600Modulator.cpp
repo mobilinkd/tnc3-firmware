a370cd36cfb426f87402bfab070deffee047bf9e
Rob Riggs <rob@pangalactic.org>
2024-09-08 12:22:09 -0700

    Update 9600 baud FSK modulator to use interpolating FIR filter rather than lookup table for pulse shaping. Allows potentially changing BT of modulator in future.

--- TNC/Fsk9600Modulator.cpp
+++ TNC/Fsk9600Modulator.cpp
@@ -5,46 +5,18 @@
 
 namespace mobilinkd { namespace tnc {
 
-/*
- * Cosine.
-const Fsk9600Modulator::cos_table_type Fsk9600Modulator::cos_table = {
-    2047,  2020,  1937,  1801,  1616,  1387,  1120,   822,   502,   169,
-    -169,  -502,  -822, -1120, -1387, -1616, -1801, -1937, -2020, -2048
-};
-*/
-
-/*
- * Square wave -- filtered in hardware at 7200Hz
-const Fsk9600Modulator::cos_table_type Fsk9600Modulator::cos_table = {
-     2047,  2047,  2047,  2047,  2047,  2047,  2047,  2047,  2047,  2047,
-    -2048, -2048, -2048, -2048, -2048, -2048, -2048, -2048, -2048, -2048
-};
-*/
-
-// Gaussian
-const Fsk9600Modulator::cos_table_type Fsk9600Modulator::cos_table = {
-    2042,  2027,  1995,  1931,  1815,  1626,  1345,   968,   507,     0,
-    -507,  -968, -1345, -1626, -1815, -1931, -1995, -2027, -2042, -2048
-};
-
 void Fsk9600Modulator::init(const kiss::Hardware& hw)
 {
     for (auto& x : buffer_) x = 2048;
 
-    (void) hw; // unused
+    UNUSED(hw);
 
     state = State::STOPPED;
-    level = Level::HIGH;
 
-    SysClock72();
-
-    // Configure 72MHz clock for 192ksps.
-    htim7.Init.Period = 374;
-    if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
-    {
-        ERROR("htim7 init failed");
-        CxxErrorHandler();
-    }
+    // Configure 72MHz clock for 96ksps.
+    
+    __HAL_TIM_SET_AUTORELOAD(&htim7, 749);
+    __HAL_TIM_SET_PRESCALER(&htim7, 0);
 
     DAC_ChannelConfTypeDef sConfig;
 
@@ -60,7 +32,24 @@ void Fsk9600Modulator::init(const kiss::Hardware& hw)
 
     if (HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 2048) != HAL_OK) CxxErrorHandler();
     if (HAL_DAC_Start(&hdac1, DAC_CHANNEL_1) != HAL_OK) CxxErrorHandler();
+    INFO("Fsk9600Modulator::init");
 }
 
+void Fsk9600Modulator::fill(uint16_t* buffer, uint8_t bits)
+{
+    HAL_IWDG_Refresh(&hiwdg);
+
+    for (uint8_t i = 0; i != BLOCKSIZE; ++i) {
+        symbols[i] = (bits & 0x80 ? UPSAMPLE : -UPSAMPLE);
+        bits <<= 1;
+    }
+
+    arm_fir_interpolate_f32(&fir_interpolator, symbols.data(), tmp, BLOCKSIZE);
+
+    for (uint8_t i = 0; i != TRANSFER_LEN; ++i)
+    {
+        buffer[i] = adjust_level(tmp[i]);
+    }
+}
 
 }} // mobilinkd::tnc
--- TNC/Fsk9600Modulator.hpp
+++ TNC/Fsk9600Modulator.hpp
@@ -4,16 +4,66 @@
 #pragma once
 
 #include "Modulator.hpp"
+#include "AudioInput.hpp"
 
 #include "stm32l4xx_hal.h"
+#include <arm_math.h>
 
 #include <array>
 #include <algorithm>
+#include <cmath>
 #include <cstdint>
 
 extern IWDG_HandleTypeDef hiwdg;
 
 namespace mobilinkd { namespace tnc {
+
+/**
+ * Generate an array of FIR filter coefficients for a Gaussian filter.
+ *
+ * @tparam TS is the symbol period, typically 1.
+ * @tparam FS is the samples per symbol (this is the upsampling factor).
+ * @tparam NSYMS is the pulse duration in symbols.
+ *
+ * @param b is the 3dB bandwidth.
+ *
+ * The filter's BT (bandwidth-time product) is TS * b.
+ *
+ * @note ARM's interpolating FIR filter requires that the number of
+ * filter coefficients be a multiple of the upsampling factor, FS.
+ */
+template <int TS, int FS, int NSYMS>
+constexpr std::array<float, TS*FS*NSYMS + FS> gaussian_filter(float b)
+{
+    // Gaussian filter.
+    std::array<float, TS*FS*NSYMS> h;
+
+    float sigma = std::sqrt(std::log(2.0f)) / (2*std::numbers::pi*b*TS);
+    float sqrt_2pi = std::sqrt(2.0f*std::numbers::pi);
+
+    for (int t = (NSYMS * TS * FS / -2), i = 0; t < (NSYMS * TS * FS / 2); ++t, ++i) {
+        float tx = t / float(FS);
+        h[i] = (1.0f / FS) / (sqrt_2pi * sigma * TS) * exp(-tx * tx / (2.0f * sigma * sigma * TS * TS));
+    }
+
+    // Rectangle of width equal to the upsampling factor, FS.
+    std::array<float, FS> rect;
+    rect.fill(1.0f);
+
+    // Convolve the Gaussian pulse with rectangle for interpolation.
+    std::array<float, TS*FS*NSYMS + FS> g;
+    g.fill(0.0f);
+    for (size_t i = 0; i != g.size() - 1; ++i) {
+        const size_t j_min = (i >= h.size() - 1) ? i - (h.size() - 1) : 0;
+        const size_t j_max = (i < rect.size() - 1) ? i : rect.size() - 1;
+        for (size_t j = j_min; j <= j_max; ++j) {
+            g[i] += (rect[j] * h[i - j]);
+        }
+    }
+
+    return g;
+}
+
 
 struct Scrambler
 {
@@ -29,15 +79,27 @@ struct Scrambler
 
 struct Fsk9600Modulator : Modulator
 {
-    static constexpr int8_t DAC_BUFFER_LEN = 40;
-    static constexpr int8_t BIT_LEN = DAC_BUFFER_LEN / 2;
-    static constexpr uint16_t VREF = 4095;
 
-    using cos_table_type = std::array<int16_t, Fsk9600Modulator::BIT_LEN>;
-    static const cos_table_type cos_table;
+    static constexpr uint8_t UPSAMPLE = 10;
+    static constexpr uint8_t BLOCKSIZE = 8;
+    static constexpr uint8_t NSYMS = 5;
+    static constexpr uint8_t TRANSFER_LEN = BLOCKSIZE * UPSAMPLE;
+    static constexpr uint16_t DAC_BUFFER_LEN = TRANSFER_LEN * 2;
+
+    static constexpr auto gaussian = gaussian_filter<1, UPSAMPLE, NSYMS>(0.5);
+
+    static constexpr uint32_t STATE_SIZE = (gaussian.size() / UPSAMPLE) + BLOCKSIZE - 1;
+    // Number of bits to flush the FIR filter.
+    static constexpr uint8_t FLUSH_LEN = 1;
+    static constexpr uint16_t VREF = 4095;
 
     enum class Level { ZERO, HIGH, LOW };
     enum class State { STOPPED, STARTING, RUNNING, STOPPING };
+
+    arm_fir_interpolate_instance_f32 fir_interpolator;
+    std::array<float, STATE_SIZE> fir_state;
+    std::array<float, BLOCKSIZE> symbols;
+    float tmp[TRANSFER_LEN];
 
     osMessageQId dacOutputQueueHandle_{0};
     PTT* ptt_{nullptr};
@@ -46,10 +108,17 @@ struct Fsk9600Modulator : Modulator
     Level level{Level::HIGH};
     State state{State::STOPPED};
     Scrambler lfsr;
+    uint8_t input_buffer_;
+    int8_t input_index_ = 0;
+    int8_t stop_count_ = 0;
 
     Fsk9600Modulator(osMessageQId queue, PTT* ptt)
     : dacOutputQueueHandle_(queue), ptt_(ptt)
-    {}
+    {
+        arm_fir_interpolate_init_f32(
+            &fir_interpolator, UPSAMPLE, gaussian.size(),
+            (float32_t*) gaussian.data(), fir_state.data(), BLOCKSIZE);
+    }
 
     ~Fsk9600Modulator() override {}
 
@@ -61,6 +130,7 @@ struct Fsk9600Modulator : Modulator
         HAL_DAC_Stop(&hdac1, DAC_CHANNEL_1);
         HAL_TIM_Base_Stop(&htim7);
         ptt_->off();
+        INFO("Fsk9600Modulator::deinit");
     }
 
     void set_gain(uint16_t level) override
@@ -87,25 +157,38 @@ struct Fsk9600Modulator : Modulator
     {
         auto scrambled = lfsr(bit);
 
+        input_buffer_ <<= 1;
+        input_buffer_ |= scrambled;
+
         switch (state)
         {
         case State::STOPPING:
         case State::STOPPED:
-            ptt_->on();
+            input_index_ = 1;
 #if defined(KISS_LOGGING) && defined(HAVE_LSCO)
-                HAL_RCCEx_DisableLSCO();
+            HAL_RCCEx_DisableLSCO();
 #endif
-
-            fill_first(scrambled);
+            osMessagePut(audioInputQueueHandle, tnc::audio::IDLE, osWaitForever);
+            fill_empty(buffer_.data());
+            fill_empty(buffer_.data() + TRANSFER_LEN);
+            ptt_->on();
+            start_conversion();
             state = State::STARTING;
             break;
         case State::STARTING:
-            fill_last(scrambled);
-            state = State::RUNNING;
-            start_conversion();
+            input_index_ += 1;
+            if (input_index_ == BLOCKSIZE) {
+                osMessagePut(dacOutputQueueHandle_, input_buffer_, osWaitForever);
+                state = State::RUNNING;
+                input_index_ = 0;
+            }
             break;
-        case State::RUNNING:
-            osMessagePut(dacOutputQueueHandle_, scrambled, osWaitForever);
+       case State::RUNNING:
+           input_index_ += 1;
+            if (input_index_ == BLOCKSIZE) {
+                osMessagePut(dacOutputQueueHandle_, input_buffer_, osWaitForever);
+                input_index_ = 0;
+            }
             break;
         }
     }
@@ -114,47 +197,78 @@ struct Fsk9600Modulator : Modulator
 
     // DAC DMA interrupt functions.
 
-    void fill_first(uint8_t bit) override
+    void fill_first(uint8_t bits) override
     {
-        fill(buffer_.data(), bit);
+        fill(buffer_.data(), bits);
     }
 
-    void fill_last(uint8_t bit) override
+    void fill_last(uint8_t bits) override
     {
-        fill(buffer_.data() + BIT_LEN, bit);
+        fill(buffer_.data() + TRANSFER_LEN, bits);
     }
 
     void empty_first() override
     {
-        empty();
+        empty(buffer_.data());
     }
 
     void empty_last() override
     {
-        empty();
+        empty(buffer_.data() + TRANSFER_LEN);
     }
 
-    void empty()
+    void empty(uint16_t* buffer)
     {
         HAL_IWDG_Refresh(&hiwdg);
-
         switch (state)
         {
         case State::STARTING:
-            // fall-through
+            fill_empty(buffer);
+            break;
         case State::RUNNING:
+            // Flush the input buffer. Because of bit-stuffing, the transmit
+            // data is not byte-aligned.
+            input_buffer_ <<= (8 - input_index_);
+
+            for (uint8_t i = 0; i != BLOCKSIZE; ++i) {
+                symbols[i] = i < input_index_ ? (input_buffer_ & 0x80 ? UPSAMPLE : -UPSAMPLE) : 0.0;
+                input_buffer_ <<= 1;
+            }
+
+            arm_fir_interpolate_f32(&fir_interpolator, symbols.data(), tmp, BLOCKSIZE);
+
+            for (uint8_t i = 0; i != TRANSFER_LEN; ++i)
+            {
+                buffer[i] = adjust_level(tmp[i]);
+            }
+
+            input_index_ = 0;
+            stop_count_ = 0;
+
             state = State::STOPPING;
             break;
         case State::STOPPING:
-            state = State::STOPPED;
+            // Flush the FIR filter.
+            symbols.fill(0.0f);
+
+            arm_fir_interpolate_f32(&fir_interpolator, symbols.data(), tmp, BLOCKSIZE);
+
+            for (uint8_t i = 0; i != TRANSFER_LEN; ++i)
+            {
+                buffer[i] = adjust_level(tmp[i]);
+            }
+
+            if (++stop_count_ == 5)
+                state = State::STOPPED;
+            break;
+        case State::STOPPED:
             stop_conversion();
             ptt_->off();
-            level = Level::HIGH;
 #if defined(KISS_LOGGING) && defined(HAVE_LSCO)
                 HAL_RCCEx_EnableLSCO(RCC_LSCOSOURCE_LSE);
 #endif
-            break;
-        case State::STOPPED:
+            osMessagePut(audioInputQueueHandle, tnc::audio::DEMODULATOR, osWaitForever);
+
             break;
         }
     }
@@ -168,6 +282,7 @@ struct Fsk9600Modulator : Modulator
 #if defined(KISS_LOGGING) && defined(HAVE_LSCO)
             HAL_RCCEx_EnableLSCO(RCC_LSCOSOURCE_LSE);
 #endif
+        INFO("Fsk9600Modulator::abort");
         // Drain the queue.
         while (osMessageGet(dacOutputQueueHandle_, 0).status == osEventMessage);
     }
@@ -204,47 +319,27 @@ private:
             DAC_ALIGN_12B_R);
     }
 
-    uint16_t adjust_level(int32_t sample) const
+    uint16_t adjust_level(float sample) const
     {
         sample *= volume_;
-        sample >>= 12;
+        sample /= 10;
         sample += 2048;
+        if (sample > 4095) sample = 4095;
+        else if (sample < 0) sample = 0;
         return sample;
     }
 
-    void fill(uint16_t* buffer, bool bit)
+    void fill(uint16_t* buffer, uint8_t bits);
+
+    [[gnu::noinline]]
+    void fill_empty(uint16_t* buffer)
     {
-        HAL_IWDG_Refresh(&hiwdg);
-        switch (level)
+        for (size_t i = 0; i != TRANSFER_LEN; ++i)
         {
-        case Level::HIGH:
-            if (bit)
-            {
-                std::fill(buffer, buffer + BIT_LEN, adjust_level(2047));
-            }
-            else
-            {
-                std::transform(cos_table.begin(), cos_table.end(), buffer,
-                    [this](auto x){return adjust_level(x);});
-                level = Level::LOW;
-            }
-            break;
-        case Level::LOW:
-            if (bit)
-            {
-                std::transform(cos_table.begin(), cos_table.end(), buffer,
-                    [this](auto x){return adjust_level(-1 - x);});
-                level = Level::HIGH;
-            }
-            else
-            {
-                std::fill(buffer, buffer + BIT_LEN, adjust_level(-2048));
-            }
-            break;
-        default:
-            CxxErrorHandler();
+            buffer[i] = 2048;
         }
     }
+
 };
 
 }} // mobilinkd::tnc
